7f2b4f4130ca21cb1532b596619921ccb4a3ad05
Luís Paulo Aganete Reis <luispaulo.aganete@hotmail.com>
2020-06-03 16:05:57 -0300

    Criando banco-tableas-atributos-script de criação

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -2,10 +2,11 @@ import knex from 'knex';
 import path from 'path';
 
 const connection = knex({
-    client: 'sqlite3', //qual banco de dados estamos usando
+    client: 'sqlite3', 
     connection: {
-        filename: path.resolve(__dirname, 'database.sqlite'), //qual vai ser o aquivo que a gente vai armazenar o db //função resolve basicamente une caminhos
+        filename: path.resolve(__dirname, 'database.sqlite'), 
     },
+    useNullAsDefault: true,
 }); 
 
-export default connection;
+export default connection; 
