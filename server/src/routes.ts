09b10557a31d26b955106dd19f29ec1c93dfcdff
Luís Paulo Aganete Reis <luispaulo.aganete@hotmail.com>
2020-06-04 10:19:13 -0300

    Crinado rota para listar os itens (script para recarregar o server mais rápido)

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,20 @@
 import express from 'express';
-
+import knex from './database/connection'
 
 const routes = express.Router(); //serve para desacoplar as rotas dos arquivos principal. P
 
-routes.get('/', (request, response) => {
-    return response.json({ message: "Hello world" });
+routes.get('/items', async (request, response) => {
+    
+    const items = await knex('items').select('*'); //SELECT * FROM items
+
+    const serializedItems =  items.map(item => { //map percorre todos os itens 
+        return {
+            title: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}`, //criando campo somente para trazer os itens
+        };
+    })
+
+    return response.json(serializedItems);
 });  
 
-export default routes;
+export default routes; 
