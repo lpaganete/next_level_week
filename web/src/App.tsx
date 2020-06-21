c1e238de79854099daaa610c76b83eb69edb14ff
Luís Paulo Aganete Reis <luispaulo.aganete@hotmail.com>
2020-06-20 22:49:54 -0300

    Aplicando conceito de STATE

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import Header from './Header';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function hendleButtonClick() {
+    setCounter(counter + 1);
+  }
+
   return (
     <div>
       <Header title="Hello World"/>
       <Header title="Hello World 2"/>
-      
 
-
-      <h1>Conteúdo da aplicação</h1>      
+      <h1>{counter}</h1>
+      <h1>{counter * 10}</h1>     
+      <button type="button" onClick={hendleButtonClick}>Aumentar</button> 
     </div>
 
   );
