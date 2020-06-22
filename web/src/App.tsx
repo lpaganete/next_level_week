73e25af6195442dbe33aef330dd8233bbedc0949
Luís Paulo Aganete Reis <luispaulo.aganete@hotmail.com>
2020-06-22 11:19:19 -0300

    Limpando projeto

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,24 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
-import Header from './Header';
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  function hendleButtonClick() {
-    setCounter(counter + 1);
-  }
 
   return (
-    <div>
-      <Header title="Hello World"/>
-      <Header title="Hello World 2"/>
-
-      <h1>{counter}</h1>
-      <h1>{counter * 10}</h1>     
-      <button type="button" onClick={hendleButtonClick}>Aumentar</button> 
-    </div>
+    <h1>Hello World</h1>
 
   );
 }
