8ae9eb1755a5e8e91000d032d16b6d39e3a4c090
Luís Paulo Aganete Reis <luispaulo.aganete@hotmail.com>
2020-06-25 13:39:00 -0300

    Criando pagina Home e configurando navegação

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,8 +1,34 @@
 import React from 'react';
+import { FiLogIn } from 'react-icons/fi';
+import { Link } from 'react-router-dom'; //* importando o link para aplicar o conceito de spa
+
+import './styles.css'
+import logo from '../../assets/logo.svg' //* importando a imagem 
+
 
 const Home = () => {
     return (
-        <h1>Hello World</h1>
+        <div id="page-home">
+            <div className="content">
+                <header>
+                    <img src={logo} alt="Ecoleta"/>     
+                </header> 
+
+                <main>
+                    <h1>Seu marketplace de coleta de resíduos.</h1>
+                    <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente</p>
+
+                    <Link  to="/create-point">
+                        <span>
+                            <FiLogIn />
+                        </span>
+                        <strong>
+                            Cadastre um ponto de coleta
+                        </strong>
+                    </Link>
+                </main>
+            </div>
+        </div>
     );
 }
 
